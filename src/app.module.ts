c6e633594097d17b531ea97ce9d8b68757397fab
Valeria Zavadetska <valeriazavadetska@Valerias-MBP.lan>
2024-10-07 21:32:30 +0300

    add docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,27 @@ import { LogIpMiddleware } from './middlewares/log-ip/logIp.middleware';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 
 @Module({
-  imports: [UsersModule, AuthModule, ConfigModule.forRoot()],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  onModuleInit() {
+    console.log('hello');
+    mongoose.connection.on('connected', () => {
+      console.log('Successfully connected to db');
+    });
+  }
+
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LogIpMiddleware).forRoutes('*');
   }
