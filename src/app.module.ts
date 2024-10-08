6e78bf76c82c276647e5bb5cc02dffdebaed0cb7
Valeria Zavadetska <valeriazavadetska@Valerias-MBP.lan>
2024-10-08 11:10:23 +0300

    fix log and add dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,26 +6,31 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRootAsync({
+      useFactory: () => {
+        return {
+          connectionFactory: (connection) => {
+            if (connection.readyState === 1) {
+              console.log('Database Connected successfully');
+            }
+            return connection;
+          },
+          uri: process.env.MONGO_URL,
+        };
+      },
+    }),
   ],
+
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
-  onModuleInit() {
-    console.log('hello');
-    mongoose.connection.on('connected', () => {
-      console.log('Successfully connected to db');
-    });
-  }
-
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LogIpMiddleware).forRoutes('*');
   }
