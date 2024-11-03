162b478d657a8c974cc0f932bce0ddc106b97666
Valeria Zavadetska <valeriazavadetska@Valerias-MBP.lan>
2024-11-03 13:27:40 +0200

    fix tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,20 +1,99 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { SignInUserDto, SignUpUserDto } from './dto';
+import { Request } from 'express';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            signUp: jest.fn(),
+            signIn: jest.fn(),
+            logout: jest.fn(),
+            refreshTokens: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('signup', () => {
+    it('should call authService.signUp with the correct data', async () => {
+      const signUpUserDto: SignUpUserDto = {
+        firstName: 'user',
+        lastName: 'clay',
+        age: 45,
+        password: 'password123',
+      };
+      const result = {
+        accessToken: 'mockToken',
+        refreshToken: 'mockRefreshToken',
+      };
+      jest.spyOn(authService, 'signUp').mockResolvedValue(result);
+
+      expect(await controller.signup(signUpUserDto)).toEqual(result);
+      expect(authService.signUp).toHaveBeenCalledWith(signUpUserDto);
+    });
+  });
+
+  describe('signin', () => {
+    it('should call authService.signIn with the correct data', async () => {
+      const signInUserDto: SignInUserDto = {
+        firstName: 'user',
+        password: 'password123',
+      };
+      const result = {
+        accessToken: 'mockToken',
+        refreshToken: 'mockRefreshToken',
+      };
+      jest.spyOn(authService, 'signIn').mockResolvedValue(result);
+
+      expect(await controller.signin(signInUserDto)).toEqual(result);
+      expect(authService.signIn).toHaveBeenCalledWith(signInUserDto);
+    });
+  });
+
+  describe('logout', () => {
+    it('should call authService.logout with the user ID', async () => {
+      const req = { user: { sub: 'user123' } } as unknown as Request;
+      jest.spyOn(authService, 'logout').mockImplementation();
+
+      await controller.logout(req);
+      expect(authService.logout).toHaveBeenCalledWith('user123');
+    });
+  });
+
+  describe('refreshTokens', () => {
+    it('should call authService.refreshTokens with the correct data', async () => {
+      const req = {
+        user: { sub: 'user123', refreshToken: 'mockRefreshToken' },
+      } as unknown as Request;
+      const result = {
+        accessToken: 'newMockToken',
+        refreshToken: 'mockRefreshToken',
+      };
+      jest.spyOn(authService, 'refreshTokens').mockResolvedValue(result);
+
+      expect(await controller.refreshTokens(req)).toEqual(result);
+      expect(authService.refreshTokens).toHaveBeenCalledWith(
+        'user123',
+        'mockRefreshToken',
+      );
+    });
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../users/users.service';
+import * as argon2 from 'argon2';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn().mockResolvedValue('mockAccessToken'),
+          },
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockImplementation((key: string) => {
+              const configMap = {
+                JWT_ACCESS_SECRET: 'mockAccessSecret',
+                JWT_ACCESS_SECRET_EXPIRE: '1h',
+                JWT_REFRESH_SECRET: 'mockRefreshSecret',
+                JWT_REFRESH_SECRET_EXPIRE: '7d',
+              };
+              return configMap[key];
+            }),
+          },
+        },
+        {
+          provide: UsersService,
+          useValue: {
+            findOneWithoutExeption: jest.fn().mockResolvedValue(null),
+            create: jest.fn().mockImplementation((user) => user),
+            findOneByName: jest.fn().mockResolvedValue({
+              id: 1,
+              firstName: 'testUser',
+              password: await argon2.hash('testPassword'),
+            }),
+            findOneAndUpdate: jest.fn().mockResolvedValue({}),
+            findOne: jest.fn().mockResolvedValue({
+              id: 1,
+              firstName: 'testUser',
+              refreshToken: await argon2.hash('mockRefreshToken'),
+            }),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
